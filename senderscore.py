8425b52ee25e71c5f91d7ec4efd46d1537733cdd
undersfx <atchin_@hotmail.com>
2019-02-16 01:36:07 -0200

    better message

--- senderscore.py
+++ senderscore.py
@@ -48,4 +48,4 @@ if __name__ == '__main__':
     score = get_senderscore(args.ip)
 
     if score:
-        print(score['score.senderscore.com'])
+        print('{} score: {}'.format(args.ip, score['score.senderscore.com']))
