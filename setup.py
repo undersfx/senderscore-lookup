39375b48fdcaf668d5b25701a2a267c032867c00
undersfx <atchin_@hotmail.com>
2021-03-13 04:31:04 -0300

    add: more options to Makefile

--- setup.py
+++ setup.py
@@ -30,7 +30,11 @@ setup(
         long_description=long_description,
         long_description_content_type="text/markdown",
         license='MIT',
-        packages=find_packages(),
+        packages=find_packages(
+            exclude=[
+                "tests",
+            ]
+        ),
         entry_points={
             'console_scripts': [
                 'senderscore = senderscore.senderscore:main'
